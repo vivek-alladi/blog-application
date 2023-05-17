9dd3bcccf4757902e78a0d74a2e9484b0f14336e
vivek-alladi <vivek.alladi26@gmail.com>
2023-05-17 15:56:26 +0530

    Add. rest api with post request for creating new resource

--- src/main/java/com/example/blogApplication/Controller/StudentController.java
+++ src/main/java/com/example/blogApplication/Controller/StudentController.java
@@ -2,10 +2,8 @@ package com.example.blogApplication.Controller;
 
 
 import com.example.blogApplication.Bean.Student;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -51,6 +49,17 @@ public class StudentController {
     @GetMapping("students/query")
     public Student studentRequestVariable(@RequestParam int id, @RequestParam String firstName, String lastName){
         return new Student(id, firstName, lastName);
+    }
+
+//    rest api that handles post request
+    @PostMapping("students/create")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Student createStudent(@RequestBody Student student){
+        System.out.println(student.getId());
+        System.out.println(student.getFirstName());
+        System.out.println(student.getLastName());
+        return student;
+
     }
 
 }
