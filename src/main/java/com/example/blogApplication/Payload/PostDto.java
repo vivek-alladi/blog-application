25283ef88b5647892663c6eb53e90ef765cd0b7b
vivek-alladi <vivek.alladi26@gmail.com>
2023-11-26 11:12:26 +0530

    Add handle specific global exceptions

--- src/main/java/com/example/blogApplication/Payload/PostDto.java
+++ src/main/java/com/example/blogApplication/Payload/PostDto.java
@@ -1,11 +1,16 @@
 package com.example.blogApplication.Payload;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Data;
 
+import java.util.Set;
+
 @Data
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class PostDto {
     private Long id;
     private String title;
     private String description;
     private String content;
+    private Set<CommentDto> comments;
 }
